f270773a9d2ab8299ab5ceea857a977990ebb4e9
sonika <sonikak004@gmail.com>
2023-01-30 15:50:05 +0100

    Modify index.js score buttons

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 let score = document.getElementById("sub-container");
+
 let num=0
-function scoreBoard(){
-    score=num+=1;
-    score.textContent=num;
+
+function scoreBoardOne(){
+    let result = num+=1;
+    score.textContent = result;
 }
+
+function scoreBoardTwo(){
+    let result = num+=2;
+    score.textContent = result;
+}
+
+function scoreBoardThree(){
+    let result = num+=3;
+    score.textContent = result;
+}
+
+let scoreTwo = document.getElementById("sub-container-two");
+
+let num2=0
+
+function scoreBoard1(){
+    let result2 = num2+=1;
+    scoreTwo.textContent = result2;
+}
+
+function scoreBoard2(){
+    let result2 = num2+=2;
+    scoreTwo.textContent = result2;
+}
+
+function scoreBoard3(){
+    let result2 = num2+=3;
+    scoreTwo.textContent = result2;
+}
+
